0bdb249cb9d60a2b07e9aa5d8944e81bd7c5376f
s124466186 <66862424+s124466186@users.noreply.github.com>
2020-06-13 14:47:23 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
-@app.route('/')
+#@app.route('/')
 def hello_world():
     return 'Hello, World! '
 @app.route('/template')
