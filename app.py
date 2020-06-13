b4dcba1a8dbb1521ffe5f9866d0b38fb2d74aace
s124466186 <66862424+s124466186@users.noreply.github.com>
2020-06-13 16:26:48 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -57,25 +57,25 @@ def movie():
 
 
 def getNews():
-	"""
-	建立一個抓最新消息的function
-	"""
-	
-
-	url = 'https://www.ettoday.net/news/focus/3C%E5%AE%B6%E9%9B%BB/'
-	r = requests.get(url)
-	reponse = r.text
-
-	url_list = re.findall(r'<h3><a href="/news/[\d]*/[\d]*.htm" .*>.*</a>',reponse)
-
-	soup = BeautifulSoup(url_list[0])
-	url = 'https://fashion.ettoday.net/' + soup.find('a')['href']
-	title = soup.text
+    """
+    建立一個抓最新消息的function
+    """
 
 
-	tmp = title + ': ' +url
-	return tmp
-	
+    url = 'https://www.ettoday.net/news/focus/3C%E5%AE%B6%E9%9B%BB/'
+    r = requests.get(url)
+    reponse = r.text
+
+    url_list = re.findall(r'<h3><a href="/news/[\d]*/[\d]*.htm" .*>.*</a>',reponse)
+
+    soup = BeautifulSoup(url_list[0])
+    url = 'https://fashion.ettoday.net/' + soup.find('a')['href']
+    title = soup.text
+
+
+    tmp = title + ': ' +url
+    return tmp
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     # 傳送文字    
@@ -90,10 +90,9 @@ def handle_message(event):
         message = TextSendMessage(getNews())
     elif event.message.text == '傳送電影:
         message = TextSendMessage(moive())
-	else:
+    else:
         message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token, message)
 
-	
 if __name__ == '__main__':
     app.run(debug=True)
